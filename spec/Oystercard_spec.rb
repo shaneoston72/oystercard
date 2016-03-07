8e383a944c0829fd7c14400cbe8e46377613f882
Shane Oston Stowe <shane@shaneoston.com>
2016-03-07 21:37:54 +0000

    Completing challenge #4 adding Oystercard_spec and oystercard.rb

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,3 +1,16 @@
-describe 'Oystercard' do
+# in spec/Oystercard_spec.rb
+require 'oystercard'
 
+describe Oystercard do
+
+  subject(:card) { described_class.new }
+
+  context 'Oystercard balance' do
+    describe '#card_balance' do
+      it "should show the card's initial balance" do
+        new_card = card
+        expect(card.balance).to eq 0
+      end
+    end
+  end
 end
