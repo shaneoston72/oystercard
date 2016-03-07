b4abcc9d6d1f02fe521289769c17af30a20433d9
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-03-07 16:38:54 +0000

    day-one challenge 4 completed

---
+++ lib/oystercard.rb
@@ -0,0 +1,8 @@
+class Oystercard
+
+def balance
+  @balance = 0  
+end
+
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,11 @@
-describe Oyster do 
+require 'oystercard'
+describe Oystercard do 
+  subject(:oystercard) { described_class.new }
+ 
+  it { is_expected.to respond_to(:balance) }
+  
+  it 'will begin with a balance of 0' do
+    expect(oystercard.balance).to eq 0
+  end
 end
+
