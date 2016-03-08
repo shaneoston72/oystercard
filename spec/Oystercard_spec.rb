abbbdcc3648ac393dc525ceb9a85100cedd19037
Shane Oston Stowe <shane@shaneoston.com>
2016-03-08 09:26:36 +0000

    Added refactored Oystercard_spec

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -6,7 +6,6 @@ describe Oystercard do
   subject(:card) { described_class.new }
 
   context 'Oystercard balance' do
-
     describe '#card_balance' do
       it "should show the card's initial balance" do
         expect(card.balance).to eq 0
@@ -14,20 +13,21 @@ describe Oystercard do
     end
 
     describe '#top_up' do
-
       it "should top up the card" do
         expect{ card.top_up 10 }.to change{ card.balance }.by 10
       end
-
       it 'should raise an error if new balance would be greater than £90' do
         max_balance = Oystercard::CARD_LIMIT
         card.top_up(max_balance)
         expect{ card.top_up 1 }.to raise_error "Card balance may not exceed £#{max_balance}"
       end
+    end
 
+    describe "#deduct" do
       it 'should reduce the balance on the card' do
         expect{ card.deduct 3 }.to change{ card.balance }.by -3
       end
     end
   end
+
 end
