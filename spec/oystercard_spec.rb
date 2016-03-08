2e76ae4e2212223b105df0ba09c541e0144ffb2f
Simon Glancy <simonglancy@Simons-MacBook-Pro.local>
2016-03-08 10:47:19 +0000

    added top-up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,19 @@
 require 'oystercard'
 
 describe Oystercard do
-  it { is_expected.to respond_to :balance }
+let(:oystercard) {described_class.new}
+
+  describe "#balance" do
+    it "1.0 has a balance of £0" do
+      expect( oystercard.balance).to eq 0
+    end
+  end
+
+  describe "#top_up" do
+    it "2.0 tops up the balance by £20" do
+      oystercard.top_up(20)
+      expect(oystercard.balance).to eq 20
+    end
+  end
+
 end
