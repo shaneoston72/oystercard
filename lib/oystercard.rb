7160c9dcbf328b92094d7b507a89c8385d6122d2
Shane Oston Stowe <shane@shaneoston.com>
2016-03-10 14:21:57 +0000

    adding morning changes

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,12 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance,  :journeys
 
   MAX_LIMIT = 90
   MIN_LIMIT = 1
 
   def initialize
     @balance = 0
+    @journeys = Array.new
   end
 
   def top_up(amount)
@@ -15,18 +16,19 @@ class Oystercard
   end
 
   def in_journey?
-    !!@in_journey
+    @journeys.select { |journey| journey.key?(:out) }.empty?
   end
 
-  def touch_in
+  def touch_in(station)
     error = "Error insufficient funds"
     raise error if @balance < MIN_LIMIT
-    @in_journey = true
+    @journeys << { :in => station }
   end
 
-  def touch_out
+  def touch_out(station)
     deduct(3)
-    @in_journey = false
+    @exit_station = station
+    @journeys.last[:out] = station
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,58 +1,80 @@
+require 'byebug'
 require 'oystercard'
 
 describe Oystercard do
-  let(:card) { described_class.new}
+  subject(:card) { described_class.new }
+  let(:station) { double(:station) }
+  let(:station2) { double(:station2) }
 
-  before do
-    card.top_up(20)
-  end
-
-  describe '#balance' do
-    it 'checks that new card has a balance' do
-      expect(subject.balance).to eq 0
+  context "#initialize" do
+    describe 'initalize#balance' do
+      it 'checks that new card has a balance' do
+        expect(card.balance).to eq 0
+      end
+      it 'has an empty list of journeys' do
+        expect(card.journeys).to be_empty
+      end
     end
   end
 
-  describe '#top_up' do
-    it 'it adds 20 to balance' do
-      expect(card.balance).to eq 20
+  context 'test context before' do
+    describe '#top_up' do
+      it 'it adds 20 to balance' do
+        card.top_up(20)
+        expect(card.balance).to eq 20
+      end
+
+      it 'raises an error if balance exceeds limit' do
+        message = "Error, balance exceeds £#{Oystercard::MAX_LIMIT}!"
+        expect{ card.top_up(100) }.to raise_error message
+      end
     end
 
-    it 'raises an error if balance exceeds limit' do
-      message = "Error, balance exceeds £#{Oystercard::MAX_LIMIT}!"
-      expect{ card.top_up(100) }.to raise_error message
-    end
-  end
+    describe '#touch_in' do
+      it 'card is in journey' do
+        card.top_up(20)
+        card.touch_in(station)
+        expect(card.in_journey?).to eq true
+      end
 
-  describe '#in_journey?' do
-    it 'card not in use' do
-      expect(card).not_to be_in_journey
-    end
-  end
+      it 'raises error when balance insufficient' do
+        message = "Error insufficient funds"
+        expect{ card.touch_in(station) }.to raise_error message
+      end
 
-  describe '#touch_in' do
-    it 'card is in journey' do
-      card.touch_in
-      expect(card.in_journey?).to eq true
-    end
+      it 'remembers entry station' do
+        card.top_up(20)
+        card.touch_in(station)
+        expect(card.journeys).to include ({ :in => station } )
+      end
 
-    it 'raises error when balance insufficient' do
-      message = "Error insufficient funds"
-      expect{ subject.touch_in }.to raise_error message
     end
 
-  end
+    describe '#touch_out' do
+      it 'card is not in journey' do
+        card.top_up(20)
+        card.touch_in(station)
+        card.touch_out(station2)
+        expect(card.in_journey?).to eq false
+      end
 
-  describe '#touch_out' do
-    it 'card is not in journey' do
-      card.touch_in
-      card.touch_out
-      expect(card.in_journey?).to eq false
+     it 'deducts fare from balance' do
+       expect{ card.touch_out(station) }.to change{ card.balance }.by -3
+     end
+
+     it 'forgets the entry station on touch out' do
+       card.top_up(20)
+       card.touch_in(station)
+       card.touch_out(station2)
+       expect(card.in_journey?).to eq false
+     end
+
+     it 'remembers exit station' do
+       card.top_up(20)
+       card.touch_in(station)
+       card.touch_out(station2)
+       expect(card.journeys).to include ({ :in => station, :out => station2 } )
+     end
     end
-
-   it 'deducts fare from balance' do
-     expect{ card.touch_out }.to change{ card.balance }.by -3
-   end
-
   end
 end
